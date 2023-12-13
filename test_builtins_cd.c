1abc7699bba898ca6de6a1c93f721eca2ea86955
HAdiiiir <hader.jaamal2010@gmail.com>
2023-12-12 23:12:29 -0800

    a

---
+++ test_builtins_cd.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char *dir = _getenv("PWD");
+	printf("Current directory: %s\n", dir);
+
+	char *args[] = {"cd", "..", NULL};
+	shell_cd(args);
+
+	dir = _getenv("PWD");
+	printf("Current directory: %s\n", dir);
+}
+	return 0;
+}
